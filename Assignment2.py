d13155d26b51a85099024e794ec9cee24dd086d8
Manoj-36 <n1k0l4t35l4369@gmail.com>
2021-09-18 11:59:50 +0530

    updates

--- Assignment2.py
+++ Assignment2.py
@@ -1,15 +1,21 @@
-from matplotlib import colors, pyplot as plt
+from matplotlib import pyplot as plt
 import pandas as pd
 
 # reading the dataset from the csv file
 data = pd.read_csv('covid19.csv')
 
 # reading data
-x1 = data.iloc[0:20,1].values
-y1 = data.iloc[0:20,2].values
+x1 = data.iloc[0:10,1].values
+y1 = data.iloc[0:10,2].values
+a1 = data.iloc[0:10,3].values
+b1 = data.iloc[0:10,4].values
 
-x2 = data.iloc[0:20,1].values
-y2 = data.iloc[0:20,3].values
+x2 = data.iloc[28:33,1].values
+y2 = data.iloc[28:33,3].values
+a2 = data.iloc[28:33,2].values
+b2 = data.iloc[28:33,4].values
+
+
 
 x3 = data.iloc[0:5,0].values
 y3 = data.iloc[0:5,1].values
@@ -20,20 +26,22 @@ a4 = data.iloc[1,1:5].values
 
 # scattering graph
 plt.subplot(221)
-plt.scatter(y1,x1)
-plt.xlabel('confirmed')
-plt.ylabel('deaths')
+plt.scatter(y1,x1,c='g',alpha=.5)
+plt.scatter(a1,b1,c='r',alpha=.5)
+plt.xlabel('confirmed & Recoverd')
+plt.ylabel('deaths & active')
 
 # plot graph
 plt.subplot(222)
 plt.plot(y2,x2,c='pink')
-plt.xlabel('confirmed')
-plt.ylabel('Recoverd')
+plt.plot(a2,b2,c='g')
+plt.xlabel('confirmed & deaths')
+plt.ylabel('Recoverd & active')
 
 # bar graph
 plt.subplot(223)
-plt.bar(x3,y3,width=0.5,label="recoverd",color="r")
-plt.bar(x3,a3,width=0.9,alpha=0.5,label="Recoverd",color="g")
+plt.bar(x3,y3,width=0.2,label="recoverd",color="r")
+plt.bar(x3,a3,width=0.4,alpha=0.5,label="Recoverd",color="g")
 plt.xlabel('Recoverd,deaths')
 plt.ylabel('Active')
 plt.legend()
--- a.py
+++ a.py
@@ -4,11 +4,11 @@ import pandas as pd
 
 data = pd.read_csv('covid19.csv')
 
-x = data.iloc[0:10,3].values
-y = data.iloc[0:10,0].values
+x = data.iloc[0:5,3].values
+y = data.iloc[0:5,0].values
 
 
-plt.bar(x,y,label='x & y')
+plt.plot(y,x,label='x & y')
 # plt.bar(x1,y1,label='x1 & y1')
 
 plt.title('Covid 19')
